5f9c55f1c0c7a3f1ca9fc0387ed7cd17143fbfa4
AlAmeen-11 <al4565292@gmail.com>
2026-06-17 11:17:23 +0530

    merged completed...

--- README.md
+++
@@ -1,2 +0,0 @@
-# HelloBook
-HelloBook is a web-based contact management system that helps users store, organize, search, and manage contacts efficiently. The system supports contact categorization, status tracking, follow-ups, and quick search features, making it suitable for both personal and professional use.
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// Import Routes
+import contactRoutes from './routes/routes.contact.js'
+
+app.use('/contacts', contactRoutes)
+
+
+// Server
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=> {
+  console.log("MongoDb connected")
+  app.listen(process.env.PORT,()=> console.log(`server running on port ${process.env.PORT}`))
+})
+.catch((err)=> console.log(err))
